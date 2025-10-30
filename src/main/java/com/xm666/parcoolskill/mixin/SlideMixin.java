8619b2b8c5a9f133a62d7c66f54ec9f5da201bba
XM666-Dev <x94732568960@163.com>
2025-10-30 09:41:08 +0800

    Fix slide animation again

--- src/main/java/com/xm666/parcoolskill/mixin/SlideMixin.java
+++ src/main/java/com/xm666/parcoolskill/mixin/SlideMixin.java
@@ -31,7 +31,9 @@ public class SlideMixin {
 
         @Inject(method = "setupRotations(Lnet/minecraft/client/player/AbstractClientPlayer;Lcom/mojang/blaze3d/vertex/PoseStack;FFFF)V", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;mulPose(Lorg/joml/Quaternionf;)V", ordinal = 2))
         private void injectPose(AbstractClientPlayer entity, PoseStack poseStack, float bob, float yBodyRot, float partialTick, float scale, CallbackInfo ci) {
-            poseStack.translate(0.0F, 0.0F, Mth.map(entity.getSwimAmount(partialTick), 1.0F, 0.0F, -0.6F, 0.0F));
+            if (SlideHandler.flipPose) {
+                poseStack.translate(0.0F, 0.0F, Mth.map(entity.getSwimAmount(partialTick), 1.0F, 0.0F, -0.6F, 0.0F));
+            }
         }
 
         //爬行恢复：在滑铲结束需要继续爬行时，将amount设为零并进行重映射，以进行平滑的过渡
