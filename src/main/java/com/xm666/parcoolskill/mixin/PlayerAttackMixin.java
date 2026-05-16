9b717ff236faaf1a737b363fc443216e8d9d7de0
XM666-Dev <x94732568960@163.com>
2026-05-16 14:15:21 +0800

    Fix skill sound

--- src/main/java/com/xm666/parcoolskill/mixin/PlayerAttackMixin.java
+++ src/main/java/com/xm666/parcoolskill/mixin/PlayerAttackMixin.java
@@ -5,11 +5,14 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import com.xm666.parcoolskill.event.PlayerAttackEvent;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.CriticalHitEvent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -17,7 +20,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 public class PlayerAttackMixin {
     @Mixin(Player.class)
-    private static class PlayerMixin {
+    private static abstract class PlayerMixin {
+        @Shadow
+        protected abstract void playServerSideSound(SoundEvent sound);
+
         @ModifyExpressionValue(method = "attack", at = @At(value = "INVOKE", target = "Lnet/neoforged/neoforge/common/CommonHooks;fireCriticalHit(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/entity/Entity;ZF)Lnet/neoforged/neoforge/event/entity/player/CriticalHitEvent;"))
         private CriticalHitEvent modifyCriticalHit(CriticalHitEvent critEvent, @Share("disableCrit") LocalBooleanRef disableCrit) {
             var playerAttackEvent = new PlayerAttackEvent.Pre(
@@ -55,7 +61,12 @@ public class PlayerAttackMixin {
 
         @ModifyArg(method = "attack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;attackVisualEffects(Lnet/minecraft/world/entity/Entity;ZZZZF)V"), index = 1)
         private boolean modifyCrit(boolean crit, @Share("disableCrit") LocalBooleanRef disableCrit) {
-            return crit && !disableCrit.get();
+            if (!crit) return false;
+
+            if (disableCrit.get()) {
+                playServerSideSound(SoundEvents.PLAYER_ATTACK_CRIT);
+            }
+            return !disableCrit.get();
         }
     }
 }
