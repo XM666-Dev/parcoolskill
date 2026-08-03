12d26417996633e07c41b520264eb761d0ca9dcd
XM666-Dev <x94732568960@163.com>
2026-08-03 19:19:11 +0800

    Fix

--- src/main/java/com/xm666/parcoolskill/mixin/parcoolskill/event/DamageBlockMixin.java
+++ src/main/java/com/xm666/parcoolskill/mixin/parcoolskill/event/DamageBlockMixin.java
@@ -1,7 +1,9 @@
 package com.xm666.parcoolskill.mixin.parcoolskill.event;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.xm666.parcoolskill.event.DamageBlockEvent;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.neoforge.common.NeoForge;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,13 +12,14 @@ import org.spongepowered.asm.mixin.injection.At;
 public class DamageBlockMixin {
     @Mixin(LivingEntity.class)
     private static class LivingEntityMixin {
-        @ModifyExpressionValue(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isDamageSourceBlocked(Lnet/minecraft/world/damagesource/DamageSource;)Z"))
-        private boolean modifyDamageSourceBlocked(boolean original) {
+        @WrapOperation(method = "hurt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isDamageSourceBlocked(Lnet/minecraft/world/damagesource/DamageSource;)Z"))
+        private boolean wrapDamageSourceBlocked(LivingEntity instance, DamageSource damageSource, Operation<Boolean> original) {
             var living = (LivingEntity) (Object) this;
-            var event = new DamageBlockEvent(living, living.damageContainers.peek(), living.damageContainers.peek().getNewDamage(), original);
+            var event = new DamageBlockEvent(living, living.damageContainers.peek(), living.damageContainers.peek().getNewDamage(), false);
             NeoForge.EVENT_BUS.post(event);
 
-            return original || event.getBlocked();
+            var value = original.call(instance, damageSource);
+            return event.getBlocked() || value;
         }
     }
 }
